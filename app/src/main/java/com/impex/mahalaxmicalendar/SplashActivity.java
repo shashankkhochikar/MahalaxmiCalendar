49d1a40d6b93be6b0846b9195f55dc29dcd7d433
Shashank Khochikar <shashank.khochikar@gmail.com>
2019-11-11 12:45:43 +0530

    Init Commit

--- app/src/main/java/com/impex/mahalaxmicalendar/SplashActivity.java
+++ app/src/main/java/com/impex/mahalaxmicalendar/SplashActivity.java
@@ -2,13 +2,27 @@ package com.impex.mahalaxmicalendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /*Intent i=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(i);
+                finish();*/
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
