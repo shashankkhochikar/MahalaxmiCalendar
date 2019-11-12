e196ce0b351ed5fbb8dcd230fe73f46a6b00d7a9
Shashank Khochikar <shashank.khochikar@gmail.com>
2019-11-12 17:01:48 +0530

    Init Commit

--- app/src/main/java/com/impex/mahalaxmicalendar/DashboardActivity.java
+++ app/src/main/java/com/impex/mahalaxmicalendar/DashboardActivity.java
@@ -1,14 +1,30 @@
 package com.impex.mahalaxmicalendar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.WindowManager;
 
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dashboard);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setIcon(R.drawable.logo_blue2);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.dashboard_menu, menu);
+        return true;
     }
 }
