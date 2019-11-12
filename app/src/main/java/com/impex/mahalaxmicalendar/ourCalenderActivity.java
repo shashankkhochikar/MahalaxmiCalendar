007c3d5ed1ba41e734642572feca2a245b7b68ac
Shashank Khochikar <shashank.khochikar@gmail.com>
2019-11-12 15:03:25 +0530

    Init Commit

---
+++ app/src/main/java/com/impex/mahalaxmicalendar/ourCalenderActivity.java
@@ -0,0 +1,14 @@
+package com.impex.mahalaxmicalendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ourCalenderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_our_calender);
+    }
+}
